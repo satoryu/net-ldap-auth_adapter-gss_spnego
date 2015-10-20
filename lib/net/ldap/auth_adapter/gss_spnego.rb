55ac141523b4c4add633aa445852d62a782f1ece
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2015-10-20 20:27:28 +0900

    Initial commit

---
+++ lib/net/ldap/auth_adapter/gss_spnego.rb
@@ -0,0 +1,11 @@
+require "net/ldap/auth_adapter/gss_spnego/version"
+
+module Net
+  module Ldap
+    module AuthAdapter
+      module GssSpnego
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/net/ldap/auth_adapter/gss_spnego/version.rb
@@ -0,0 +1,9 @@
+module Net
+  module Ldap
+    module AuthAdapter
+      module GssSpnego
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
